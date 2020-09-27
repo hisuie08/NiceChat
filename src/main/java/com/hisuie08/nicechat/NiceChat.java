8389cd7675f5554a9a002a7e036e8cef49bb48f3
HISUIE <32667550+hisuie08@users.noreply.github.com>
2020-09-27 20:40:25 +0900

    cmt

--- src/main/java/com/hisuie08/nicechat/NiceChat.java
+++ src/main/java/com/hisuie08/nicechat/NiceChat.java
@@ -1,15 +1,20 @@
 package com.hisuie08.nicechat;
 
 
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.text.ChatType;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 
 @Mod(NiceChat.MODID)
 public class NiceChat
@@ -19,21 +24,23 @@ public class NiceChat
     public static final Logger LOGGER = LogManager.getLogger();
 
     public NiceChat() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
         MinecraftForge.EVENT_BUS.register(this);
     }
-
-    private void setup(final FMLCommonSetupEvent event) {
-    }
-
     private void doClientStuff(final FMLClientSetupEvent event) {}
+    @SubscribeEvent
+    public void onKeyToConfig(InputEvent.KeyInputEvent event){
+        LOGGER.info(event.getKey());
+    }
+    @SubscribeEvent
+    public void onChatReceived(ClientChatReceivedEvent event){
+//        NiceChat.LOGGER.info("Server: " + event.getMessage());
+        if(event.getType() == ChatType.CHAT){
+            NiceChat.LOGGER.info(event.getMessage());
+            event.setMessage(ITextComponent.func_244388_a("well!"));
 
-    private void enqueueIMC(final InterModEnqueueEvent event) {}
+        }
 
-    private void processIMC(final InterModProcessEvent event) {}
+    }
 
 }
--- src/main/java/com/hisuie08/nicechat/NiceConfig.java
+++ src/main/java/com/hisuie08/nicechat/NiceConfig.java
@@ -1,30 +1,14 @@
 package com.hisuie08.nicechat;
 
-import me.shedaniel.clothconfig2.forge.api.ConfigBuilder;
-import me.shedaniel.clothconfig2.forge.api.ConfigCategory;
-import me.shedaniel.clothconfig2.forge.api.ConfigEntryBuilder;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.util.text.ITextComponent;
-
-import java.util.concurrent.atomic.AtomicReference;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.config.ModConfig;
 
 
-public class NiceConfig {
-    public NiceConfig(Screen parent) {
-        ConfigBuilder builder = ConfigBuilder.create()
-                .setParentScreen(parent)
-                .setTitle(ITextComponent.func_244388_a("title."+NiceChat.MODID+".config"));
-        builder.setSavingRunnable(() -> {
-            // Serialise the config into the config file. This will be called last after all variables are updated.
-        });
-        ConfigCategory general = builder.getOrCreateCategory(ITextComponent.func_244388_a("category."+NiceChat.MODID+".general"));
-        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-        AtomicReference<String> currentValue = new AtomicReference<>("abc");
-        general.addEntry(entryBuilder.startStrField(ITextComponent.func_244388_a("option.examplemod.optionA"), currentValue.get())
-                .setDefaultValue("This is the default value") // Recommended: Used when user click "Reset"
-                .setTooltip(ITextComponent.func_244388_a("This option is awesome!")) // Optional: Shown when the user hover over this option
-                .setSaveConsumer(newValue -> currentValue.set(newValue)) // Recommended: Called when user save the config
-                .build()); // Builds the option entry for cloth config
+public class NiceConfig extends ModConfig {
 
+    public static ForgeConfigSpec CLIENT;
+    public NiceConfig(Type type, ForgeConfigSpec spec, ModContainer container, String fileName) {
+        super(type, spec, container, fileName);
     }
 }
