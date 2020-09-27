3e9d7e1ec5cef15abd70578ea0b7eb2caf4c9246
HISUIE <32667550+hisuie08@users.noreply.github.com>
2020-09-27 23:59:08 +0900

    cmt

--- src/main/java/com/hisuie08/nicechat/NiceChat.java
+++ src/main/java/com/hisuie08/nicechat/NiceChat.java
@@ -1,45 +1,52 @@
 package com.hisuie08.nicechat;
 
 
-import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.client.Minecraft;
 import net.minecraft.util.text.ChatType;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
-import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.loading.FMLConfig;
+import net.minecraftforge.fml.loading.FMLPaths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 
 @Mod(NiceChat.MODID)
 public class NiceChat
 {
     public static final String MODID = "nicechat";
-
     public static final Logger LOGGER = LogManager.getLogger();
 
-    public NiceChat() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+
+    public NiceChat() throws IOException {
         MinecraftForge.EVENT_BUS.register(this);
-    }
-    private void doClientStuff(final FMLClientSetupEvent event) {}
-    @SubscribeEvent
-    public void onKeyToConfig(InputEvent.KeyInputEvent event){
-        LOGGER.info(event.getKey());
+        NiceConfig.Init();
     }
     @SubscribeEvent
-    public void onChatReceived(ClientChatReceivedEvent event){
+    public void onChatReceived(ClientChatReceivedEvent event) throws FileNotFoundException {
 //        NiceChat.LOGGER.info("Server: " + event.getMessage());
         if(event.getType() == ChatType.CHAT){
             NiceChat.LOGGER.info(event.getMessage());
-            event.setMessage(ITextComponent.func_244388_a("well!"));
-
+            List ng = new NiceConfig().load();
+            if(ng.contains(event.getMessage().getString())) {
+                event.setMessage(ITextComponent.func_244388_a("well!"));
+            }
         }
+        LOGGER.info(Minecraft.getInstance().gameDir.getName());
 
     }
 
--- src/main/java/com/hisuie08/nicechat/NiceConfig.java
+++ src/main/java/com/hisuie08/nicechat/NiceConfig.java
@@ -1,14 +1,34 @@
 package com.hisuie08.nicechat;
 
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.ModContainer;
-import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.loading.FMLPaths;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class NiceConfig extends ModConfig {
+public class NiceConfig {
+    public static File NiceConfigDir = new File(FMLPaths.CONFIGDIR.get().toFile(),"NiceChat");
+    public static File ignoreWordsFile = new File(NiceConfigDir,"/ignores.txt");
+    public static List<String> ignoreList = new ArrayList<>();
 
-    public static ForgeConfigSpec CLIENT;
-    public NiceConfig(Type type, ForgeConfigSpec spec, ModContainer container, String fileName) {
-        super(type, spec, container, fileName);
+    public static void Init() throws IOException {
+        if(!NiceConfigDir.exists()){
+            NiceConfigDir.mkdirs();
+            NiceChat.LOGGER.info(NiceConfigDir);
+        }
+        if(!ignoreWordsFile.exists()){
+            ignoreWordsFile.createNewFile();
+        }
+    }
+    public static List<String> load() throws FileNotFoundException {
+        try (BufferedReader br = new BufferedReader(new FileReader(ignoreWordsFile))) {
+            String text;
+            while ((text = br.readLine()) != null) {
+                ignoreList.add(text);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ignoreList;
     }
 }
